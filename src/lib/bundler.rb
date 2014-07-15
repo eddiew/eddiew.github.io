c00546e8d8a1a85117ae10c8b14adb57ba5ad025
unknown <wangeddx@WANGEDDX-MOBL.amr.corp.intel.com>
2014-07-15 16:24:04 -0700

    Split js and css into components, add concatenation

--- src/lib/bundler.rb
+++ src/lib/bundler.rb
@@ -1,8 +1,11 @@
 def all_js(files)
   js_arr = []
   for file in files
-    item = @items['/js/#{file}/']
-    puts "File #{file} doesn't exist!" unless item
+    item = @items.find{|i| i.identifier == "/js/#{file}/"}
+    unless item
+      puts "Script #{file} doesn't exist!"
+      next
+    end
     js_arr << item.compiled_content
   end
   js_arr.join("\n")
@@ -11,8 +14,11 @@ end
 def all_css(files)
   css_arr = []
   for file in files
-    item = @items['/css/#{file}/']
-    puts "File #{file} doesn't exist!" unless item
+    item = @items.find{|i| i.identifier == "/css/#{file}/"}
+    unless item
+      puts "Stylesheet #{file} doesn't exist!"
+      next
+    end
     css_arr << item.compiled_content
   end
   css_arr.join("\n")
