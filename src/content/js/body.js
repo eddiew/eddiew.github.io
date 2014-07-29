06f6c90a43d8c116964708b362ca1ee6bd97a797
unknown <wangeddx@WANGEDDX-MOBL.amr.corp.intel.com>
2014-07-29 15:26:59 -0700

    more about progress, misc js fixes

--- src/content/js/body.js
+++ src/content/js/body.js
@@ -1,9 +1,9 @@
 "use strict";
 // Global variables
-var $html_body = $('html, body'),
-	$window = $(window),
+var $html_body,
+	$window,
 	curSection,
-	navLinks = [];
+	navLinks = {};
 
 // SCROLL
 // TODO: Mobile
@@ -29,55 +29,60 @@ function scrollToTarget(target) {
 	scrollTo(scrollTarget);
 }
 
-$('#nav').find('a').each(function(idx) {
-	var link = $(this),
-		target = $(link.attr('href')),
-		ref = link.attr('href').substring(1); // href without the '#'
-	if (idx === 0) {
-		curSection = ref;
-		link.addClass('active').
-		click(function(event) {
-			event.preventDefault();
-			scrollTo(0);
-		});
-	}
-	else link.click(function(event) {
-			event.preventDefault();
-			scrollToTarget(target);
-		});
-	navLinks[ref] = link;
-});
+$(function() {
+	$html_body = $('html, body');
+	$window = $(window);
 
-// Listener to set active nav link
-// TODO: un-fragile this + mobile
-$window.scroll(function() {
-	// Get section at middle of the window
-	var elem = document.elementFromPoint(221, $window.height() / 2); // 221 is nav width + 1
-	if (elem.id === curSection) return;
-	// Set new active link
-	navLinks[curSection].removeClass('active');
-	curSection = elem.id;
-	navLinks[curSection].addClass('active');
-});
+	$('#navlist').find('a').each(function(idx) {
+		var link = $(this),
+			target = $(link.attr('href')),
+			ref = link.attr('href').substring(1); // href without the '#'
+		if (idx === 0) {
+			curSection = ref;
+			link.addClass('active').
+			click(function(event) {
+				event.preventDefault();
+				scrollTo(0);
+			});
+		}
+		else link.click(function(event) {
+				event.preventDefault();
+				scrollToTarget(target);
+			});
+		navLinks[ref] = link;
+	});
 
-// HELLO
-var $home = $('#home'),
-	$go = $('#go'),
-	$about = $('#about'),
-	homeHeight = parseInt($home.innerHeight(), 10),
-	minHomeHeight = parseInt($home.css('min-height'), 10),
-	aboutHeight = homeHeight + parseInt($('about').innerHeight() / 2 - $window.height() / 2, 10);
+	// Listener to set active nav link
+	// TODO: un-fragile this + mobile
+	$window.scroll(function() {
+		// Get section at middle of the window
+		var elem = document.elementFromPoint(221, $window.height() / 2); // 221 is nav width + 1
+		if (elem.id === curSection) return;
+		// Set new active link
+		navLinks[curSection].removeClass('active');
+		curSection = elem.id;
+		if (navLinks[curSection] != null) navLinks[curSection].addClass('active');
+	});
 
-// Scroll button control
-$go.click(function(event) {
-	event.preventDefault();
-	scrollToTarget($about);
-});
+	// HELLO SLIDE
+	var $home = $('#home'),
+		$go = $('#go'),
+		$about = $('#about'),
+		homeHeight = parseInt($home.innerHeight(), 10),
+		minHomeHeight = parseInt($home.css('min-height'), 10),
+		aboutHeight = homeHeight + parseInt($('about').innerHeight() / 2 - $window.height() / 2, 10);
 
-$window.scroll(function() {
-	if ($window.scrollTop() > aboutHeight) $go.addClass('inactive');
-	else $go.removeClass('inactive');
-	var shrinkAmt = Math.min(homeHeight - minHomeHeight, $window.scrollTop());
-	$home.innerHeight(homeHeight - shrinkAmt);
-	$home.css('margin-top', shrinkAmt);
+	// Scroll button control
+	$go.click(function(event) {
+		event.preventDefault();
+		scrollToTarget($about);
+	});
+
+	$window.scroll(function() {
+		if ($window.scrollTop() > aboutHeight) $go.addClass('inactive');
+		else $go.removeClass('inactive');
+		var shrinkAmt = Math.min(homeHeight - minHomeHeight, $window.scrollTop());
+		$home.innerHeight(homeHeight - shrinkAmt);
+		$home.css('margin-top', shrinkAmt);
+	});
 });
